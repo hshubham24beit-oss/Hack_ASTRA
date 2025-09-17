f4dd0acf22a49eff001c990f2e968d987ea9a608
Shubham <hshubham24beit@student.mes.ac.in>
2025-09-17 23:17:29 +0530

    Updated homepage / changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 mongoose
   .connect(
     process.env.MONGO_URI ||
-      "mongodb+srv://hshubham24beit_db_user:Shubham9769517001@cluster0.plupwfh.mongodb.net/votechain?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://hshubham24beit_db_user:hshubham24beit_db_user@cluster0.plupwfh.mongodb.net/votechain?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
